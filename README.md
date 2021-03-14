c6f37c5d7e84f7adc5e78ebd8bf6c5705da26f4e
Guillaume Andreys <guillaume.andreys@code.famira.fr>
2021-03-14 17:41:27 +0100

    Refonte complète du projet avec utilisation d'un script python

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # WebSite monitoring exemple
 
-Mise en place d'un monitoring de site web à l'aide d'{Elastic Heartbeat}(https://www.elastic.co/fr/beats/heartbeat). Les données des sites web sont remontés dans une base {Elastic Search}(https://www.elastic.co/fr/elasticsearch/) et un dashboard {Grafana}(https://grafana.com/) permet de visualiser les données et de générer des alertes.
+Mise en place d'un monitoring de site avec enregistrement dans une base de données {Elasticseach}(https://www.elastic.co/fr/elasticsearch/), visualisation dans un dashboard  {Grafana}(https://grafana.com/) et remontée des alertes via WebHook. 
+
 Cette stack est lancée et préconfigurée dans un ensemble de container docker lancés via docker compose.
 
 ## Prérequis
@@ -10,7 +11,7 @@ Docker et docker compose doivent être présent.
 ## Lancement
 
 Se positionner dans le répertoire *Docker*
-Exécuter la commande *docker-compose up*
+Exécuter la commande ```docker-compose up```
 
 On peut se connecter à Grafana sur http://localhost:80 avec admin/admin
 
@@ -19,34 +20,58 @@ Dans la démo, nous effectuons le monitoring sur deux sites :
 - famira.fr/test.php : Ce site contient un petit script php qui va de manière aléatoire prendre du temps pour répoudre un 200 ou un 500
 
 Dans Grafana nous retrouvons deux graphique sur chaque site :
-- Un graphique permettant de visualiser le temps de réponse du site. Une alerte est levée si le site met trop de temps à répondre. Cette alerte est volontairement basse pour la démo.
-- Un graphique permettant de voir quand le site est UP (Réponse 200) ou DOWN (Réponse 500 ou timeout). Une alerte est levée si le site est trop souvent DOWN. Cette alerte est volontairement basse pour la démo.
+- Un graphique permettant de visualiser le temps de réponse du site. 
+- Un graphique permettant de voir quand le site est UP (Réponse 200) ou DOWN (Réponse 500 ou timeout).
 
-Les alertes envoie une notificatin du type webhook consultables vers : https://webhook.site/#!/693630a3-7a32-4986-a7d2-5fb272d8b7c0
+En cas de non réponse d'un site ou de réponse autre que success, une alerte de type webhook consultables vers : https://webhook.site/#!/693630a3-7a32-4986-a7d2-5fb272d8b7c0
 
 ## Configuration
 
 ### Configuration des sites à monitorer
 
-La configuration des sites à monitorer se situe dans le fichier *docker/heartbeat.docker.yml*. Se référer à la documentation de Heartbeat : https://www.elastic.co/guide/en/beats/heartbeat/current/configuring-howto-heartbeat.html
+La configuration des sites à monitorer se situe dans le fichier *app/site_monitoring.yml*.
 
-Après modification de la configuration de heartbeat il est nécessaire de rebuilder l'image en lançant un *docker-compose build* avant de relancer les container via *docker-compose up*
+Configuration globale à tous les sites à surveiller :
+
+```
+conf:
+  #Interval between each check in second
+  check_interval: 15
+  #Host of the Elasticseach instance to save data
+  elasticsearch_host: 'elasticsearch:9200'
+  #url of the WebHook to raise the alert
+  alert_webhook_url: https://webhook.site/693630a3-7a32-4986-a7d2-5fb272d8b7c0
+```
+
+Configuration des sites :
+
+```
+sites:
+  - famira_status:
+      #Timeout of the request in second
+      timeout: 2
+      #Url of the site to monitor
+      url: https://famira.fr/test.php
+      #Description of the site to monitor
+      description: "Famira Status"
+      alert:
+        #If we want to raise an alert if site response is not success or timeout
+        raise_alert: true
+        #Text to send with the alert
+        alert_text: 'Famira website is slow or down'
+```
 
 L'ajout d'un nouveau site à monitorer demande également l'ajout des différents graphiques et alertes dans Grafana.
-
-### Configuration des alertes
-
-Les alertes sont configurées directement dans Grafana.
-
-L'ajout d'alertes de type mail nécessite de configurer un serveur smpt dans le fichier */grafana/grafana.ini*.
 
 ## Limitations et amélioration
 
 Ce projet n'est absolument pas utilisable tel quel en production. Il manque notamment toute la gestion de la sécurité (Mot de passe par défaut ...).
 
-Les seuils d'alerte sont définis pour lever des alertes plus fréquement que l'on voudrait dans un vrais projet. Ceci à des fins de tests.
+Le script de monitoring manque de gestion d'erreur et les logs sont à émaliorer (Niveau de log).
+
+La configuration du site de monitoring n'est pas rechargée automatiquement
 
 Les dashboard Grafana nécessiteraient des améliorations, notamment l'utilisation de template.
 
-On pourrait améliorer la gestion de la configuration de heartbeat pour ne pas avoir à rebuilder l'image en créant un volume qui fait pointer le fichier de configuration du docker vers le fichier de configuration local mais j'ai été confronté à un problème de droits.
+De nombreuses amélioration à apporter sur la gestion des alertes : Envoyer les alertes seulement si le site est en erreur pendant un certain temps, permettre l'envoi vers d'autre format (mail, slack ...)
 
---
+++ app/site_monitoring.py
@@ -0,0 +1,145 @@
+import requests as r
+import time
+import yaml
+import datetime
+import json
+import os
+from elasticsearch import Elasticsearch
+from elasticsearch import ElasticsearchException
+
+class WebSiteStatusResult:
+    def __init__(self, status, request_time, reason):
+        self.status = status
+        self.request_time = request_time
+        self.reason= reason
+
+    def statusToString(self):
+        if self.status:
+            return 'up'
+        else:
+            return 'down'
+
+    def toString(self):
+            return f"'status': '{self.statusToString()}', 'request_time': {str(self.request_time)}, 'reason': {self.reason}"
+
+class SiteMonitoringConfig:
+    def __init__(self, url, timeout, id, description):
+        self.url = url
+        self.timeout = timeout
+        self.id = id
+        self.description = description
+        self.alert = False
+        self.alert_text = ''
+
+    def setAlertForSite(self, alert, alert_text):
+        self.alert = alert
+        self.alert_text = alert_text
+
+    def isValid(self):
+        return self.url and self.id and self.timeout
+
+    def toString(self):
+        return f'Url : {self.url}, id : {self.id}, description: {self.description}, timeout: {self.timeout}'
+
+class SiteMonitoringApp:
+    def readConfigFile(self, file):
+        with open(file, "r") as yamlfile:
+            print("Reading config file ", file)
+            conf_data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+            if (not 'conf' in conf_data): 
+                raise Exception("Fichier " + file + " mal formé, conf absent du fichier") 
+
+            if (not 'sites' in conf_data): 
+                raise Exception("Fichier " + file + " mal formé, sites absent du fichier") 
+
+            if type(conf_data['conf']) == type(None):
+                raise Exception("Fichier " + file + " mal formé, conf non renseigné") 
+
+            self.check_interval = conf_data['conf'].get('check_interval', 15)
+            self.elasticsearch_host = conf_data['conf'].get('elasticsearch_host', 'localhost:9200')
+            self.alert_webhook_url = conf_data['conf'].get('alert_webhook_url')
+
+            print("Check interval : ", self.check_interval, 's')
+            print("Elasticsearch host : ", self.elasticsearch_host)
+                       
+            if type(conf_data['sites']) == type(None):
+                raise Exception("Fichier " + file + " mal formé, aucun site de configuré ") 
+
+            self.sites = []
+            for site in conf_data['sites']:
+                site_id = list(site.keys())[0]
+                site_config = site[site_id]
+                site_monitoring_config = SiteMonitoringConfig(site_config.get('url', ''), site_config.get('timeout', ''), site_id, site_config.get('description', ''))
+                if site_monitoring_config.isValid():
+                    self.sites.append(site_monitoring_config)
+                    print('Ajout du site ', site_id, ' dans le monitoring avec les paramètres ', site_monitoring_config.toString())
+                    if ('alert' in site_config) and (site_config['alert']['raise_alert'] == True):
+                        site_monitoring_config.setAlertForSite(True, site_config['alert']['alert_text'])
+                        print("Ajout d'une alert pour", site_id)
+                else:
+                    print("Configuration invalide pour le site", site_id, ". Site ignoré.")
+
+    def check_website_avalability(self, host, timeout):
+        try:
+            startTime = time.time()
+            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+            req = r.request('GET', host, headers=headers,timeout=timeout)
+            endTime = time.time()
+            request_time = round((endTime - startTime) * 1000)
+            return WebSiteStatusResult(req.ok, request_time, req.status_code)
+        except r.exceptions.RequestException as e:
+            return WebSiteStatusResult(False, -1, str(e))
+
+    def write_check_result_to_elastic(self, site, check_resut):
+        doc = {
+            '@timestamp': datetime.datetime.now(),
+            'url': site.url,
+            'id': site.id,
+            'description': site.description,
+            'status': check_resut.statusToString(),
+            'request_time': check_resut.request_time
+        }
+        try:
+            self.es.index(index='site_monitoring', body=doc)
+        except ElasticsearchException as e:
+            print("Erreur lors de l'écriture dans Elasticsearch : ", e)
+
+    def check_and_send_alert(self, site, check_result):
+        if site.alert and not check_result.status:
+            payload = {
+                'url':site.url,
+                'description':site.description,
+                'id': site.id,
+                'text': site.alert_text,
+                'reason': check_result.reason
+            }
+            try:
+                print("Sending alert to", self.alert_webhook_url)
+                r.post(self.alert_webhook_url, json=payload)
+            except r.exceptions.RequestException as e:
+                print("Error send alert to", self.alert_webhook_url, ":",e)
+
+
+    def execute(self):
+        while True:
+            for site in self.sites:       
+                print("Checking", site.url)
+                check_result = self.check_website_avalability(site.url, site.timeout)
+
+                print(check_result.toString())
+                self.write_check_result_to_elastic(site, check_result)
+                self.check_and_send_alert(site, check_result)
+
+            time.sleep(self.check_interval)
+
+    def __init__(self, conf_file):
+        self.readConfigFile(conf_file)
+        self.es = Elasticsearch(
+            [self.elasticsearch_host]
+        )
+
+conf_file = os.getenv('CONF_FILE', 'site_monitoring.yml')
+app = SiteMonitoringApp(conf_file)
+app.execute()
+
